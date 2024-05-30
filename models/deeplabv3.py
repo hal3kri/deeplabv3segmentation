70b61a0defc0739e2c4bb1a92f7032a217fafc88
hal3kri <167310427+hal3kri@users.noreply.github.com>
2024-05-30 11:20:02 +0200

    Update deeplabv3.py

--- models/deeplabv3.py
+++ models/deeplabv3.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
+from models.aspp import build_aspp
+
 
 class Deeplabv3(nn.Module):
   def __init__(self, output_stride = 16, num_classes
